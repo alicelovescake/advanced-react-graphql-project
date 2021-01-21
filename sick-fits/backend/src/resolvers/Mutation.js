15a40270cfa1c24ae61fd56968a479cc340b0ea5
Alice Zhao <azhao991@gmail.com>
2021-01-21 13:50:34 -0800

    Create data model, prisma and schema for graphql & set up server

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,26 @@
-const Mutations = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
+    console.log(ctx.db);
+    const item = await ctx.db.mutation.createItem(
+      // returns a promise but no value stored into item unless you put async
+      {
+        data: {
+          ...args,
+        },
+      },
+      info // ensures item is returned to us from database once we we've created it. info has query.
+    );
+    return item;
+    //database in put into context
+  },
+  //   createDog(parent, args, ctx, info) {
+  //     global.dogs = global.dogs || [];
+  //     //create a dog
+  //     const newDog = { name: args.name };
+  //     global.dogs.push(newDog);
+  //     return newDog;
+  //   },
+};
 
-module.exports = Mutations;
+module.exports = Mutation;
